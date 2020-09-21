bb13dd113ab9f3e03f2e49446d5952567381e8e2
Denis Sergeevich <denissuspicin@gmail.com>
2020-09-21 19:40:35 +0300

    Initial Commit

---
+++ MyLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by DenisSuspitsyn on 21.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
